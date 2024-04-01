21966d054d8d7ede164703be56e970afd6ce92b7
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-01 20:08:51 +0530

    changed 4 files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Contacts from "./scenes/contacts";
 import EditUser from "./scenes/contacts/EditUser";
 import ViewUser from "./scenes/contacts/ViewUser";
 import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
+import Signup from "./scenes/signup/index.jsx";
 import { ColorModeContext, useMode } from "./theme";
-
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === "/";
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar isSidebar={isSidebar} />
+          {!isLoginPage && <Sidebar isSidebar={isSidebar} />}
           <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
+            {!isLoginPage && <Topbar setIsSidebar={setIsSidebar} />}
             <Routes>
-              <Route path="/" element={<Dashboard />} />
-
+              <Route path="/" element={<Signup />} />
+              <Route path="/dd" element={<Dashboard />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/contacts/view/:id" element={<ViewUser />} />
               <Route path="/contacts/edit/:id" element={<EditUser />} />
